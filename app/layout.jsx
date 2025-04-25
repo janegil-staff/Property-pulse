b918ab434592314e58bf86dde26360e88ed7b662
Jan Egil Staff Øvretveit <janegilstaffovretveit@Mac.lan>
2025-04-25 16:24:57 +0200

    Initial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "Property Pulse",
+  keywordds: "rental, property, real estate",
+  description: "Find the perfect rental property"
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
